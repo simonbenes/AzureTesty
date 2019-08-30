18207ba47d7e11502ccc1331a4d4fbd5e8167e83
simonbenes <simon.benes@gmail.com>
2019-08-31 00:58:33 +0200

    Update script1.ps1

--- testapp/script1.ps1
+++ testapp/script1.ps1
@@ -15,7 +15,7 @@ Param (
 # Firewall
 netsh advfirewall firewall add rule name="http" dir=in action=allow protocol=TCP localport=80
 netsh advfirewall firewall add rule name="rdp" dir=in action=allow protocol=TCP localport=3389
-netsh advfirewall firewall add rule name="test" dir=in action=allow protocol=TCP localport=1234
+netsh advfirewall firewall add rule name="test" dir=in action=deny protocol=TCP localport=1234
 
 # Folders
 New-Item -ItemType Directory c:\temp
@@ -26,23 +26,23 @@ Install-WindowsFeature web-server -IncludeManagementTools
 
 
 # Install dot.net core sdk
-#Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=615460 -outfile c:\temp\vc_redistx64.exe
-#Start-Process c:\temp\vc_redistx64.exe -ArgumentList '/quiet' -Wait
-#Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
-#Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
-#Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
-#Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest http://go.microsoft.com/fwlink/?LinkID=615460 -outfile c:\temp\vc_redistx64.exe
+Start-Process c:\temp\vc_redistx64.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkID=809122 -outfile c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe
+Start-Process c:\temp\DotNetCore.1.0.0-SDK.Preview2-x64.exe -ArgumentList '/quiet' -Wait
+Invoke-WebRequest https://go.microsoft.com/fwlink/?LinkId=817246 -outfile c:\temp\DotNetCore.WindowsHosting.exe
+Start-Process c:\temp\DotNetCore.WindowsHosting.exe -ArgumentList '/quiet' -Wait
 
 # Download music app
-#Invoke-WebRequest  https://github.com/Microsoft/dotnet-core-sample-templates/raw/master/dotnet-core-music-windows/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
-#Expand-Archive C:\temp\musicstore.zip c:\music
+Invoke-WebRequest  https://github.com/Microsoft/dotnet-core-sample-templates/raw/master/dotnet-core-music-windows/music-app/music-store-azure-demo-pub.zip -OutFile c:\temp\musicstore.zip
+Expand-Archive C:\temp\musicstore.zip c:\music
 
 # Azure SQL connection sting in environment variable
 [Environment]::SetEnvironmentVariable("Data:DefaultConnection:ConnectionString", "Server=$sqlserver;Database=MusicStore;Integrated Security=False;User Id=$user;Password=$password;MultipleActiveResultSets=True;Connect Timeout=30",[EnvironmentVariableTarget]::Machine)
 
 # Pre-create database
 $env:Data:DefaultConnection:ConnectionString = "Server=$sqlserver;Database=MusicStore;Integrated Security=False;User Id=$user;Password=$password;MultipleActiveResultSets=True;Connect Timeout=30"
-#Start-Process 'C:\Program Files\dotnet\dotnet.exe' -ArgumentList 'c:\music\MusicStore.dll'
+Start-Process 'C:\Program Files\dotnet\dotnet.exe' -ArgumentList 'c:\music\MusicStore.dll'
 
 # Configure iis
 Remove-WebSite -Name "Default Web Site"
